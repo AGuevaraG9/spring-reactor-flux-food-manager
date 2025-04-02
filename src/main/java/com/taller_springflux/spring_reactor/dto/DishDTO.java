1cb34f918b83df80e54657aa0a3295f8566f7306
Abel Guevara <55357845+AbelG9@users.noreply.github.com>
2025-04-02 01:25:05 -0500

    feat: added exception error handler

--- src/main/java/com/taller_springflux/spring_reactor/dto/DishDTO.java
+++ src/main/java/com/taller_springflux/spring_reactor/dto/DishDTO.java
@@ -1,9 +1,6 @@
 package com.taller_springflux.spring_reactor.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,12 +10,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class DishDTO {
     private String id;
+    @NotNull(message = "Attribute nameDishes must not be null")
     @Size(min = 2, max = 20)
+    @Pattern(regexp = "^[A-Za-záéíóúñÁÉÍÓÚÑ ]+$", message = "Must comply with the pattern")
     private String nameDishes;
     @NotNull
-    @Min(value = 1)
-    @Max(value = 999, message = "The value must be under or equal to 999")
+    //@Min(value = 1)
+    //@Max(value = 999, message = "The value must be under or equal to 999")
+    @DecimalMin(value = "0.5")
+    @DecimalMax(value = "999.99")
     private double priceDishes;
     @NotNull
     private boolean statusDishes;
+    @NotNull
+    @Email
+    private String email;
 }
